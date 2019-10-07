65eafb1348085656f17fb9721b0c30ee4bc2bd54
eduardofmonteiro <dudmont1@gmail.com>
2019-10-07 17:10:42 -0300

    english version

---
+++ circle_calculator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace circle_calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Circle calculator!");
+
+            Console.WriteLine("Enter cicle radius:");
+            string radi = Console.ReadLine();
+            decimal radid = Convert.ToDecimal(radi);
+
+            decimal area = 0;
+
+            area =
+                3.141592m * (radid * radid);
+
+            Console.WriteLine("The area of the circle is: " + area);
+        }
+    }
+}
