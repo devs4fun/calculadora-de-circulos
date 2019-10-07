4bcd62e26e2c948c9401d054a86ac726d003cd79
eduardofmonteiro <dudmont1@gmail.com>
2019-10-07 16:11:46 -0300

    codigo

---
+++ calculadora_de_circulos/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace calculadora_de__quadrados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculadora de quadarados!");
+
+            Console.WriteLine("Insira o raio do quadrado:");
+            string raio = Console.ReadLine();
+            decimal raiod = Convert.ToDecimal(raio); 
+
+            decimal area = 0;
+
+            area = 
+                3.141592m * (raiod * raiod); 
+
+            Console.WriteLine("A area do circulo é de "+area);
+           
+
+        }
+        
+    }
+}
